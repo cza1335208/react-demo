e86965c4f280bedf0d21e878971f29ae0b7d3727
chenzhiang <1019656899@qq.com>
2025-12-28 05:52:20 +0800

    test: eslint

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0) 
   let eslintTest1 = '1111';
-  let eslintTest2 = 1111234673663333
+  let eslintTest2 = 11112346736633332
   return (
     <>
       <div>
